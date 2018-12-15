a96cd09486326a8e5ce008857125c467f910fbfc
Kevin <kevinpwong@live.com>
2018-12-15 13:01:30 -0800

    heroku debugging

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static( __dirname + '/public/dist/public/' ));
 
 app.all("*", (req,res,next) => {
-    res.sendFile(path.resolve("./public/dist/public/index.html"))
+    res.sendFile(path.resolve(path.join(__dirname+"/public/dist/public/index.html")))
   });
 
 app.listen(port, function() {
