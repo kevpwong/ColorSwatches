e4c4af0c5e4373d0b15a8c150627a639321caef3
Kevin <kevinpwong@live.com>
2018-12-15 13:08:25 -0800

    heroku pt2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static( __dirname + '/public/dist/public/' ));
